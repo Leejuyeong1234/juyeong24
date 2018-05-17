b9d8f2a306401fb710390247e7ab1158617df7f5
M9203-21\user <zzzskf1234@naver.com>
2018-05-17 11:45:54 +0900

    프로젝트 비주얼노벨  자료

---
+++ Novel/Assets/NOVEl/Script/DialogueParser.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEditor;
+
+public class DialogueParser : MonoBehaviour
+{
+    struct DialogueLine
+    {
+        public string name;
+        public string content;
+        public int pose;
+        public string position;
+        public string[] options;
+
+        public DialogueLine(string Name, string Content, int Pose, string Position)
+        {
+            name = Name;
+            content = Content;
+            pose = Pose;
+            position = Position;
+            options = new string[0];
+        }
+    }
+
+    List<DialogueLine> lines;
+
+    // Use this for initialization
+    void Start()
+    {
+        string file = "Assets/Data/Dialogue";
+        string sceneNum = EditorApplication.currentScene;
+        sceneNum = Regex.Replace(sceneNum, "[^0-9]", "");
+        file += sceneNum;
+        file += ".txt";
+
+        lines = new List<DialogueLine>();
+        LoadDialogue(file);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LoadDialogue(string filename)
+    {
+        string line;
+        StreamReader r = new StreamReader(filename);
+
+        using (r)
+        {
+            do
+            {
+                line = r.ReadLine();
+                if (line != null)
+                {
+                    string[] lineData = line.Split(';');
+                    if (lineData[0] == "Player")
+                    {
+                        DialogueLine lineEntry = new DialogueLine(lineData[0], "", 0, "");
+                        lineEntry.options = new string[lineData.Length - 1];
+                        for (int i = 1; i < lineData.Length; i++)
+                        {
+                            lineEntry.options[i - 1] = lineData[i];
+                        }
+                        lines.Add(lineEntry);
+                    }
+                    else
+                    {
+                        DialogueLine lineEntry = new DialogueLine(lineData[0], lineData[1], int.Parse(lineData[2]), lineData[3]);
+                        lines.Add(lineEntry);
+                    }
+                }
+            }
+            while (line != null);
+            r.Close();
+        }
+    }
+
+    public string GetPosition(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].position;
+        }
+        return "";
+    }
+
+    public string GetContent(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].content;
+        }
+        return "";
+    }
+    public string[] GetOptions(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].options;
+        }
+        return new string[0];
+    }
+}
+
---
+++ Novel/Assets/NOVEl/Script/SceneSelectButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelectButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadSceneSelect()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+}
